93ef4c41f8f0db5d1c7690093f686754e5a555c0
BurkinGunke <burkingunke@hotmail.com>
2018-04-26 18:09:15 +0200

    V2I with working granurality in simulator.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -8,91 +8,123 @@ import org.apache.curator.framework.state.ConnectionState;
 import org.apache.curator.framework.state.ConnectionStateListener;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
 
 public class main {
-	
-    public static void main(String args[]) {
+	private static int maxWait = 9999;
 
-        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+	@SuppressWarnings("null")
+	public static void main(String args[]) {
 
-        String hostInput = null;
-        String node = null;
-        int sections_needed = 0;
-        InterProcessMutex k1;
-    	InterProcessMutex k2;
-    	InterProcessMutex k3;
-    	InterProcessMutex k4;
-    	int maxWait = 9999;
-        
-        if (args.length > 0) {
-            hostInput = args[0];
-        }
+		RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
 
-        String zookeeperConnectionString = hostInput != null ? hostInput : "localhost:2181";
-        CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
+		String hostInput = null;
+		String node = null;
+		int sections_needed = 0;
+		String initial_direction = null;
+		InterProcessMutex k1 = null;
+		InterProcessMutex k2 = null;
+		InterProcessMutex k3 = null;
+		InterProcessMutex k4 = null;
+		List<InterProcessMutex> list_of_locks = new ArrayList<InterProcessMutex>();
 
-        client.start();
+		if (args.length > 0) {
+			hostInput = args[0];
+		}
 
-        /* --------------------------------
-         * Listen for connection changes
-         * -------------------------------- **/
-        Listenable<ConnectionStateListener> csl = client.getConnectionStateListenable();
-        csl.addListener(new ConnectionStateListener() {
-            @Override
-            public void stateChanged(CuratorFramework client, ConnectionState newState) {
-                System.out.println("Connection state: ");
-                if (newState.isConnected()) {
-                    System.out.println("zk_connection_successful");
-                } else {
-                    System.out.println("zk_connection_failed");
-                }
-            }
-        });
-        
-        //Throw exception here maybe? If there is no argument[1] the node will be named "null"
-        if (args.length > 1) {
-            node = args[1];
-            //sections_needed = Integer.parseInt(args[2]); //I hope you gave me an integer...
-        }
-        
-        if (true) {
-        	String lockPath = "/" + node;
-        	k1 = new InterProcessMutex(client, lockPath+"/k1");
-        	k2 = new InterProcessMutex(client, lockPath+"/k2");
-        	k3 = new InterProcessMutex(client, lockPath+"/k3");
-        	k4 = new InterProcessMutex(client, lockPath+"/k4");
-        	
-        	try {
-                if (k1.acquire(maxWait, TimeUnit.SECONDS) &&
-                		k2.acquire(maxWait, TimeUnit.SECONDS) &&
-                		k3.acquire(maxWait, TimeUnit.SECONDS) &&
-                		k4.acquire(maxWait, TimeUnit.SECONDS)) {
-                    try {
-                        System.out.println("lock_accepted");
+		System.out.println(hostInput);
 
-                        // waiting for input before releasing lock
-                        int i = System.in.read();
-                        System.out.println(i);
-                    } finally {
-                        k1.release();
-                        k2.release();
-                        k3.release();
-                        k4.release(); 
-                    }
-                }
-            } catch (Exception e) {
-                System.out.println("Could not acquire lock");
-                System.out.println(e.getMessage());
-            }
-        }
-       
+		String zookeeperConnectionString = hostInput != null ? hostInput : "localhost:2181";
+		CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy);
 
-        
+		/*
+		 * -------------------------------- Listen for connection changes
+		 * --------------------------------
+		 **/
+		Listenable<ConnectionStateListener> csl = client.getConnectionStateListenable();
+		csl.addListener(new ConnectionStateListener() {
+			@Override
+			public void stateChanged(CuratorFramework client, ConnectionState newState) {
+				System.out.println("Connection state: ");
+				if (newState.isConnected()) {
+					System.out.println("zk_connection_successful");
+				} else {
+					System.out.println("zk_connection_failed");
+				}
+			}
+		});
 
-        
-        
-    }
+		client.start();
+
+		// Throw exception here maybe? If there is no argument[1] the node will be named
+		// "null"
+		if (args.length > 1) {
+			node = args[1];
+			initial_direction = args[2];
+			sections_needed = Integer.parseInt(args[3]); // I hope you gave me an integer...
+
+		}
+
+		String lockPath = "/" + node;
+
+		if (sections_needed == 4) {
+			list_of_locks.add(k1 = new InterProcessMutex(client, lockPath + "/k1"));
+			list_of_locks.add(k2 = new InterProcessMutex(client, lockPath + "/k2"));
+			list_of_locks.add(k3 = new InterProcessMutex(client, lockPath + "/k3"));
+			list_of_locks.add(k4 = new InterProcessMutex(client, lockPath + "/k4"));
+		} 
+		else if (sections_needed == 2 && initial_direction.equals("right")) {
+			list_of_locks.add(k3 = new InterProcessMutex(client, lockPath + "/k3"));
+			list_of_locks.add(k4 = new InterProcessMutex(client, lockPath + "/k4"));
+		} 
+		else if (sections_needed == 2 && initial_direction.equals("left")) {
+			list_of_locks.add(k1 = new InterProcessMutex(client, lockPath + "/k1"));
+			list_of_locks.add(k2 = new InterProcessMutex(client, lockPath + "/k2"));
+		}else if (sections_needed == 1) {
+			list_of_locks.add(k1 = new InterProcessMutex(client, lockPath));
+		}
+		
+		try {
+			if (list_of_locks.size()> 0 && locks_aquired(list_of_locks)) {
+				System.out.println("lock_accepted");
+
+				// waiting for input before releasing lock
+				int i = System.in.read();
+				System.out.println(i);
+				for(InterProcessMutex lock : list_of_locks) {
+					lock.release();
+				}
+			}
+
+		} catch (Exception e) {
+			System.out.println("Could not acquire lock");
+			System.out.println(e.getMessage());
+		}
+		//In case you move around the truck, the truck could be waiting for a continue signal
+		//That's why we send one here. It won't effect the intersection management. 
+		System.out.println("lock_accepted"); 
+		System.exit(0);
+
+	}
+
+	public static boolean locks_aquired(List<InterProcessMutex> list_of_locks) {
+		boolean all_locks_aquired = true;
+
+		for (InterProcessMutex lock : list_of_locks) {
+			try {
+				if (!lock.acquire(maxWait, TimeUnit.SECONDS)) {
+					all_locks_aquired = false;
+				}
+			} catch (Exception e) {
+				System.out.println("Could not acquire lock");
+				System.out.println(e.getMessage());
+			}
+		}
+		return all_locks_aquired;
+
+	}
 }
